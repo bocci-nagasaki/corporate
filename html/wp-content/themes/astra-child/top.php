966cf8318b7ae9bd2d248c33e012788d5855c83d
erugis0345 <tl091264@gmail.com>
2020-04-16 00:32:20 +0900

    init

---
+++ html/wp-content/themes/astra-child/top.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: custom-top-template
+*/
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+get_header(); ?>
+
+<?php if (astra_page_layout() == 'left-sidebar') : ?>
+    <?php get_sidebar(); ?>
+<?php endif ?>
+
+<div id="primary" <?php astra_primary_class(); ?>>
+    <?php astra_primary_content_top(); ?>
+    <?php astra_content_loop(); ?>
+    <?php the_content() ?>
+    <?php astra_primary_content_bottom(); ?>
+</div><!-- #primary -->
+
+<?php if (astra_page_layout() == 'right-sidebar') : ?>
+    <?php get_sidebar(); ?>
+<?php endif ?>
+
+<?php get_footer(); ?>
