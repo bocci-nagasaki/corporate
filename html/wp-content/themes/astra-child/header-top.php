0ea31bc2fb5db70ed32194aa2f447b4f2066c59e
erugis0345 <tl091264@gmail.com>
2020-04-16 00:59:07 +0900

    update

--- html/wp-content/themes/astra-child/header-top.php
+++ html/wp-content/themes/astra-child/header-top.php
@@ -10,50 +10,50 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 ?><!DOCTYPE html>
 <?php astra_html_before(); ?>
 <html <?php language_attributes(); ?>>
 <head>
-<?php astra_head_top(); ?>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php astra_head_top(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
-<?php astra_head_bottom(); ?>
+    <?php wp_head(); ?>
+    <?php astra_head_bottom(); ?>
 </head>
 
 <body <?php astra_schema_body(); ?> <?php body_class(); ?>>
 
 <?php astra_body_top(); ?>
 <?php wp_body_open(); ?>
-<div 
-	<?php
-	echo astra_attr(
-		'site',
-		array(
-			'id'    => 'page',
-			'class' => 'hfeed site',
-		)
-	);
-	?>
+<div
+    <?php
+    echo astra_attr(
+        'site',
+        array(
+            'id' => 'page',
+            'class' => 'hfeed site',
+        )
+    );
+    ?>
 >
-	<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?></a>
+  <a class="skip-link screen-reader-text" href="#content">
+      <?php echo esc_html(astra_default_strings('string-header-skip-link', false)); ?>
+  </a>
 
-	<?php astra_header_before(); ?>
+    <?php astra_header_before(); ?>
+    <?php include(ABSPATH . 'wp-content/themes/astra-child/top-page-header.php'); ?>
+    <?php astra_header_after(); ?>
 
-	<?php astra_header(); ?>
+    <?php astra_content_before(); ?>
 
-	<?php astra_header_after(); ?>
+  <div id="content" class="site-content">
 
-	<?php astra_content_before(); ?>
+    <div class="ast-container">
 
-	<div id="content" class="site-content">
-
-		<div class="ast-container">
-
-		<?php astra_content_top(); ?>
+        <?php astra_content_top(); ?>
---
+++ html/wp-content/themes/astra-child/top-page-header.php
@@ -0,0 +1,75 @@
+<header
+  class="site-header ast-primary-submenu-animation-fade header-main-layout-1 ast-primary-menu-enabled ast-logo-title-inline ast-hide-custom-menu-mobile ast-menu-toggle-icon ast-mobile-header-inline"
+  id="masthead"
+  itemtype="https://schema.org/WPHeader"
+  itemscope="itemscope"
+>
+  <div class="main-header-bar-wrap">
+    <div class="main-header-bar">
+      <div class="ast-container">
+        <div class="ast-flex main-header-container">
+          <div class="site-branding">
+            <div class="ast-site-identity" itemtype="https://schema.org/Organization" itemscope="itemscope">
+              <div class="ast-site-title-wrap">
+                <h1 class="site-title" itemprop="name">
+                  <a href="http://localhost:8080/" rel="home" itemprop="url">bocci corporate site</a>
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div class="ast-mobile-menu-buttons">
+            <div class="ast-button-wrap">
+              <button
+                type="button"
+                class="menu-toggle main-header-menu-toggle ast-mobile-menu-buttons-minimal"
+                aria-controls="primary-menu"
+                aria-expanded="false"
+                data-index="0"
+              >
+                <span class="screen-reader-text">メインメニュー</span>
+                <span class="menu-toggle-icon"></span>
+              </button>
+            </div>
+          </div>
+
+          <div class="ast-main-header-bar-alignment">
+            <div class="main-header-bar-navigation">
+              <nav
+                class="site-navigation"
+                id="site-navigation"
+                itemtype="https://schema.org/SiteNavigationElement"
+                itemscope="itemscope"
+                aria-label="サイトナビゲーション"
+              >
+                <div id="primary-menu" class="main-navigation">
+                  <ul class="main-header-menu ast-nav-menu ast-flex ast-justify-content-flex-end  submenu-with-border astra-menu-animation-fade">
+                    <li class="page_item page-item-1 menu-item">
+                      <a href="#product-process">生産工程</a>
+                      <button class="ast-menu-toggle" aria-expanded="false">
+                        <span class="screen-reader-text">メニュートグル</span>
+                      </button>
+                    </li>
+                    <li class="page_item page-item-2 menu-item">
+                      <a href="#producer">生産者</a>
+                      <button class="ast-menu-toggle" aria-expanded="false">
+                        <span class="screen-reader-text">メニュートグル</span>
+                      </button>
+                    </li>
+                    <li class="page_item page-item3 menu-item">
+                      <a href="#contact-us">お問い合わせ</a>
+                      <button class="ast-menu-toggle" aria-expanded="false">
+                        <span class="screen-reader-text">メニュートグル</span>
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
