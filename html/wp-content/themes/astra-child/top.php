0a4f7e6499b3dc50e2cdf0cff4e7207b20147563
erugis0345 <tl091264@gmail.com>
2020-04-16 19:22:24 +0900

    install bootstrap

--- html/wp-content/themes/astra-child/top.php
+++ html/wp-content/themes/astra-child/top.php
@@ -8,22 +8,48 @@ if (!defined('ABSPATH')) {
 get_header('top');
 ?>
 
-<video autoplay loop poster="polina.jpg">
-<!--  <source src="polina.webm" type="video/webm">-->
-  <source src="/wp-content/uploads/2020/01/kv.mp4" type="video/mp4">
-</video>
+<video autoplay loop muted src="/wp-content/uploads/2020/01/kv.mp4"></video>
 
-<div id="primary" <?php astra_primary_class(); ?>>
-    <?php
-    astra_primary_content_top();
-    astra_content_loop();
-    the_content();
-    astra_primary_content_bottom();
-    ?>
+<div class="container">
+  <section id="product-process" style="text-align: center">
+    <h2>生産工程</h2>
+    <p>
+      親譲りの無鉄砲で小供の時から損ばかりしている。<br>
+      小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。<br>
+      なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。<br>
+      新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。
+    </p>
+  </section>
+
+  <section id="producer" style="text-align: center">
+    <h2>生産者</h2>
+  </section>
+
+  <section id="contact-us" style="text-align: center">
+    <h2>お問い合わせ</h2>
+    <ul>
+      <li>おなまえ</li>
+      <li>れんらくさき</li>
+      <li>ないよう</li>
+    </ul>
+  </section>
 </div>
 
-<?php if (astra_page_layout() == 'right-sidebar') {
-    get_sidebar();
-}
-get_footer();
-?>
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+  crossorigin="anonymous"
+></script>
+<script
+  src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+  crossorigin="anonymous"
+></script>
+<script
+  src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+  crossorigin="anonymous"
+></script>
+</body>
+</html>
