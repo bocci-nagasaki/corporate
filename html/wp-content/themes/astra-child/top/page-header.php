13adc24c3121a9d4ae621a0d6996ce71ffb33a9f
erugis0345 <tl091264@gmail.com>
2020-04-16 23:16:17 +0900

    modify navbar design

--- html/wp-content/themes/astra-child/top/page-header.php
+++ html/wp-content/themes/astra-child/top/page-header.php
@@ -1,24 +1,24 @@
 <nav id="navbar" class="navbar navbar-light bg-light sticky-top justify-content-center">
   <ul class="nav nav-pills">
     <li class="nav-item pc">
-      <a class="nav-link" href="#what-is-dejima">デジマとは？</a>
+      <a class="nav-link p-2 text-dark" href="#what-is-dejima">デジマとは？</a>
     </li>
     <li class="nav-item pc">
-      <a class="nav-link" href="#secret-of-taste">おいしいヒミツ</a>
+      <a class="nav-link p-2 text-dark" href="#secret-of-taste">おいしいヒミツ</a>
     </li>
     <a class="navbar-brand" href="#">
       <img src="/wp-content/uploads/static/potato-logo.png" alt="" id="logo" height="40">
     </a>
     <li class="nav-item pc">
-      <a class="nav-link" href="#producer">生産者</a>
+      <a class="nav-link p-2 text-dark" href="#producer">生産者</a>
     </li>
     <li class="nav-item pc">
-      <a class="nav-link" href="#contact-us">お問い合わせ</a>
+      <a class="nav-link p-2 text-dark" href="#contact-us">お問い合わせ</a>
     </li>
       <?php // For smartphone ?>
     <li class="nav-item dropdown sp">
       <a
-        class="nav-link dropdown-toggle"
+        class="nav-link dropdown-toggle text-dark"
         data-toggle="dropdown"
         href="#"
         role="button"
@@ -26,10 +26,10 @@
         aria-expanded="false"
       >Dropdown</a>
       <div class="dropdown-menu">
-        <a class="dropdown-item" href="#what-is-dejima">デジマとは？</a>
-        <a class="dropdown-item" href="#secret-of-taste">おいしいヒミツ</a>
-        <a class="dropdown-item" href="#producer">生産者</a>
-        <a class="dropdown-item" href="#contact-us">お問い合わせ</a>
+        <a class="dropdown-item text-dark" href="#what-is-dejima">デジマとは？</a>
+        <a class="dropdown-item text-dark" href="#secret-of-taste">おいしいヒミツ</a>
+        <a class="dropdown-item text-dark" href="#producer">生産者</a>
+        <a class="dropdown-item text-dark" href="#contact-us">お問い合わせ</a>
       </div>
     </li>
   </ul>
