7b83976c45a89b44832f9c6e89489ca489ed4ef4
erugis0345 <tl091264@gmail.com>
2020-04-17 00:09:07 +0900

    background

--- html/wp-content/themes/astra-child/top/body.php
+++ html/wp-content/themes/astra-child/top/body.php
@@ -3,30 +3,40 @@
 
 <div class="container">
 
-  <div>
-    <div class="mb-3 pt-5" id="what-is-dejima"></div>
-      <?php include(ABSPATH . 'wp-content/themes/astra-child/top/what-is-dejima.php'); ?>
+  <div class="mb-3 pt-5" id="what-is-dejima"></div>
+  <div class="card cover">
+    <div class="card-body">
+        <?php include(ABSPATH . 'wp-content/themes/astra-child/top/what-is-dejima.php'); ?>
+    </div>
   </div>
 
-  <div>
-    <div class="mb-3 pt-5" id="secret-of-taste"></div>
-      <?php include(ABSPATH . 'wp-content/themes/astra-child/top/secret-of-taste.php'); ?>
+  <div class="mb-3 pt-5" id="secret-of-taste"></div>
+  <div class="card cover">
+    <div class="card-body">
+        <?php include(ABSPATH . 'wp-content/themes/astra-child/top/secret-of-taste.php'); ?>
+    </div>
   </div>
 
-  <div>
-    <div class="mb-3 pt-5" id="producer"></div>
-      <?php include(ABSPATH . 'wp-content/themes/astra-child/top/producer.php'); ?>
+  <div class="mb-3 pt-5" id="producer"></div>
+  <div class="card cover">
+    <div class="card-body">
+        <?php include(ABSPATH . 'wp-content/themes/astra-child/top/producer.php'); ?>
+    </div>
   </div>
 
   <div class="mb-3 pt-5" id="contact-us"></div>
-  <h2 class="mb-4"><span class="marker-yellow">お問い合わせ</span></h2>
-  <div class="row" style="margin-bottom: 400px;">
-    <div class="col-sm">
-      <ul>
-        <li>おなまえ</li>
-        <li>れんらくさき</li>
-        <li>ないよう</li>
-      </ul>
+  <div class="card cover">
+    <div class="card-body">
+      <h2 class="mb-4"><span class="marker-yellow">お問い合わせ</span></h2>
+      <div class="row">
+        <div class="col-sm">
+          <ul>
+            <li>おなまえ</li>
+            <li>れんらくさき</li>
+            <li>ないよう</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </div>
--- html/wp-content/themes/astra-child/top/secret-of-taste.php
+++ html/wp-content/themes/astra-child/top/secret-of-taste.php
@@ -1,47 +1,37 @@
 <h2 class="mb-4"><span class="marker-yellow">おいしいヒミツ</span></h2>
 <div class="row">
 
-    <div class="col-sm">
-        <div class="card h-100 border-0 mb-2">
-            <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
-            </div>
-        </div>
+  <div class="col-sm">
+    <div class="card h-100 border-0 mb-2">
+      <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
     </div>
+  </div>
 
-    <div class="col-sm">
-        <div class="card h-100 border-0 mb-2">
-            <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
-            </div>
-        </div>
+  <div class="col-sm">
+    <div class="card h-100 border-0 mb-2">
+      <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
     </div>
+  </div>
 
-    <div class="col-sm">
-        <div class="card h-100 border-0 mb-2">
-            <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
-            </div>
-        </div>
+  <div class="col-sm">
+    <div class="card h-100 border-0 mb-2">
+      <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
     </div>
-
-    <div class="col-sm">
-        <div class="card h-100 border-0 mb-2">
-            <img src="/wp-content/uploads/static/bad.png" class="card-img-top" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
-            </div>
-        </div>
-    </div>
+  </div>
 </div>
