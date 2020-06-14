78c7935275238e090d60c4e2df9b9b5bc20f0e1b
erugis0345 <tl091264@gmail.com>
2020-06-14 16:35:58 +0900

    add mobile style

--- html/wp-content/themes/astra-child/top/secret-of-taste.php
+++ html/wp-content/themes/astra-child/top/secret-of-taste.php
@@ -1,19 +1,19 @@
 <h2 class="mb-4"><span class="marker-yellow">おいしいヒミツ</span></h2>
 <div class="row">
 
-  <div class="col-sm">
+  <div class="col-sm mb-4">
     <div class="card h-100 border-0 mb-2" style="background-color: transparent;">
       <img src="/wp-content/uploads/static/top/secret-of-taste-01.png" class="card-img-top" alt="サラブレット品種の馬鈴薯">
     </div>
   </div>
 
-  <div class="col-sm">
+  <div class="col-sm mb-4">
     <div class="card h-100 border-0 mb-2" style="background-color: transparent;">
       <img src="/wp-content/uploads/static/top/secret-of-taste-02.png" class="card-img-top" alt="70年続く馬鈴薯栽培の歴史">
     </div>
   </div>
 
-  <div class="col-sm">
+  <div class="col-sm mb-4">
     <div class="card h-100 border-0 mb-2" style="background-color: transparent;">
       <img src="/wp-content/uploads/static/top/secret-of-taste-03.png" class="card-img-top" alt="厳選された特別な畑での栽培">
     </div>
