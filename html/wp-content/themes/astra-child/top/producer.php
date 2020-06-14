05fc73e6d382fe260b4eb520847b4b0d3d0b707d
erugis0345 <tl091264@gmail.com>
2020-06-14 16:01:12 +0900

    replace producer

--- html/wp-content/themes/astra-child/top/producer.php
+++ html/wp-content/themes/astra-child/top/producer.php
@@ -1,39 +1,55 @@
 <h2 class="mb-4"><span class="marker-yellow">生産者</span></h2>
 <div class="row">
-    <div class="col-sm">
-        <ul class="list-unstyled">
-            <li class="media">
-                <img src="/wp-content/uploads/static/producer.png" width="200" class="mr-3 bg-light rounded-circle" alt="...">
-                <div class="media-body">
-                    <h5 class="mt-0 mb-1">Aさん</h5>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                    ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cill
-                </div>
-            </li>
-            <li class="media my-4">
-                <div class="media-body">
-                    <h5 class="mt-0 mb-1">Bさん</h5>
-                    つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、
-                    そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）
-                    つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、
-                    そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）
-                </div>
-                <img src="/wp-content/uploads/static/producer.png" width="200" class="mr-3 bg-light rounded-circle" alt="...">
-            </li>
-            <li class="media">
-                <img src="/wp-content/uploads/static/producer.png" width="200" class="mr-3 bg-light rounded-circle" alt="...">
-                <div class="media-body">
-                    <h5 class="mt-0 mb-1">Cさん</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-                    scelerisque ante sollicitudin. Cras purus odio,
-                    vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum
-                    nunc ac nisi vulputate fringilla. Donec
-                    lacinia congue felis in faucibus.
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div class="col-sm">
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="/wp-content/uploads/static/top/producer-01.png" width="200" class="mr-3" alt="生産者「永尾教志」の画像">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">永尾教志</h5>
+          <p>
+            <b>この味を知らないなんてもったいない！是非一度食べてほしい！</b><br>
+            「デジマ」という品種は耳にしたことはありましたが食べたことがありませんでした。<br>
+            先輩方の栽培を始め、今まで食べたことのないような美味しいその味にすっかり虜になりました。<br>
+            自信たっぷりの「デジマ」、ぜひ食べてほしい一品です。
+          </p>
+        </div>
+      </li>
+      <li class="media my-4">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">松尾幸輝</h5>
+          <p>
+            <b>こんなに喜ばれる「デジマ」を未来へ繋げたい！</b><br>
+            祖父(二代目)時代からデジマ栽培を始めており、親しい友人や親戚に送るなど大変喜ばれるじゃがいもです。<br>
+            今では作る農家が減っており、数少ない希少なデジマです。<br>
+            息子も作ってくれるような自慢の「デジマ」をこれからも作り続けます。
+          </p>
+        </div>
+        <img src="/wp-content/uploads/static/top/producer-02.png" width="200" class="mr-3" alt="生産者「松尾幸輝」の画像">
+      </li>
+      <li class="media">
+        <img src="/wp-content/uploads/static/top/producer-03.png" width="200" class="mr-3" alt="生産者「渡部賢吾」の画像">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">渡部賢吾</h5>
+          <p>
+            <b>先人から現代へ受け継がれた歴史を守りたい！</b><br>
+            私が暮らす南串山町は急勾配な土地のため、雲仙の山麓でありながら海を間近に見渡せ、自然の恩恵をたくさん受けられるとても良い所です。<br>
+            先人がこの土地には馬鈴薯が最適だとたどり着き、「デジマ」という品種が美味しさでは最適だと現代人が確立しました。<br>
+            これからも未来へ、品質や味を追求しながらデジマを守って行きます。
+          </p>
+        </div>
+      </li>
+      <li class="media my-4">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">本田健吾</h5>
+          <p>
+            <b>最高の環境で育った最高のじゃがいも「デジマ」！技術と愛情も込めました！</b><br>
+            南串山町の土壌は水はけ良く適度な保水性と排水性を持ち、ミネラルと肥沃性の絶妙なバランス、そして海風と冬場の太陽エネルギーを効率よく受ける段々畑で、じゃがいもはストレスなくすくすく育つことが出来ます。<br>
+            そんな恵まれた環境でも消費者の皆様に喜んで頂く為、作りやすさよりも美味しさを追究し風味、食感、共に抜群の「デジマ」という品種に挑戦しています。<br>
+            私たちの技術と愛情、色んな思いの詰まった「デジマ」をどうぞご賞味ください。
+          </p>
+        </div>
+        <img src="/wp-content/uploads/static/top/producer-04.png" width="200" class="mr-3" alt="生産者「本田健吾」の画像">
+      </li>
+    </ul>
+  </div>
 </div>
