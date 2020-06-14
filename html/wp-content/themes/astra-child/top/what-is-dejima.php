19c044e982b979286f22dfb3c75dd0c86b53171d
erugis0345 <tl091264@gmail.com>
2020-06-14 15:47:55 +0900

    replace what-is-dejima

--- html/wp-content/themes/astra-child/top/what-is-dejima.php
+++ html/wp-content/themes/astra-child/top/what-is-dejima.php
@@ -2,14 +2,14 @@
 <div class="row">
     <div class="col-sm">
         <div class="card">
-            <img src="/wp-content/uploads/static/what-is-dejima.png" class="card-img-top rounded-lg shadow" alt="...">
+            <img src="/wp-content/uploads/static/top/what-is-dejima.png" class="card-img-top rounded-lg shadow" alt="地面から掘り出したばかりの美味しそうなじゃがいもの画像">
         </div>
     </div>
     <div class="col-sm">
         <p>
-            「デジマ」という品種は、秋に収穫される最も美味しいジャガイモと言われています。<br>
-            しかし、栽培が非常に難しく、収量がとれません。<br>
-            なので、地元の農家の方々は商品としてではなく、自らが食べるため、また親しい方への贈り物として栽培しており、市場にはあまり出回らない幻の品種です。
+          デジマという品種は秋に収穫される、最も美味しいジャガイモと言われています。<br>
+          しかし栽培が非常に難しいことに加えて収穫量が少ないため、地元の農家さんは商品としてではなく親しい方への贈り物としております。<br>
+          市場にはあまり出回らない幻の品種です。
         </p>
     </div>
 </div>
