73836c1ca4b3d5b4466d3943f753b61ee9ef64f4
erugis0345 <tl091264@gmail.com>
2020-06-17 20:40:55 +0900

    fix design

--- html/wp-content/themes/astra-child/top/producer.php
+++ html/wp-content/themes/astra-child/top/producer.php
@@ -2,7 +2,7 @@
 <div class="row d-none d-sm-block">
   <div class="col-sm">
     <ul class="list-unstyled">
-      <li class="media mb-4 pb-4 border-bottom">
+      <li class="media mb-4 pb-4 border-bottom producer-hr">
         <img src="/wp-content/uploads/static/top/producer-01.png" width="200" class="mr-3" alt="生産者「永尾教志」の画像">
         <div class="media-body">
           <h5 class="mt-0 mb-1">永尾教志</h5>
@@ -14,7 +14,7 @@
           </p>
         </div>
       </li>
-      <li class="media my-4 mb-4 pb-4 border-bottom">
+      <li class="media my-4 mb-4 pb-4 border-bottom producer-hr">
         <div class="media-body">
           <h5 class="mt-0 mb-1">松尾幸輝</h5>
           <p><b>こんなに喜ばれる「デジマ」を未来へ繋げたい！</b></p>
@@ -26,7 +26,7 @@
         </div>
         <img src="/wp-content/uploads/static/top/producer-02.png" width="200" class="mr-3" alt="生産者「松尾幸輝」の画像">
       </li>
-      <li class="media mb-4 pb-4 border-bottom">
+      <li class="media mb-4 pb-4 border-bottom producer-hr">
         <img src="/wp-content/uploads/static/top/producer-03.png" width="200" class="mr-3" alt="生産者「渡部賢吾」の画像">
         <div class="media-body">
           <h5 class="mt-0 mb-1">渡部賢吾</h5>
@@ -38,7 +38,7 @@
           </p>
         </div>
       </li>
-      <li class="media my-4 mb-4 pb-4 border-bottom">
+      <li class="media my-4 mb-4 pb-4 border-bottom producer-hr">
         <div class="media-body">
           <h5 class="mt-0 mb-1">本田健吾</h5>
           <p><b>最高の環境で育った最高のじゃがいも「デジマ」！技術と愛情も込めました！</b></p>
